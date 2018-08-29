c99697a124cde145e6ff9374621d878b7f223a28
Aakash Sarang <sarangaakash@gmail.com>
2018-08-29 16:47:41 -0700

    Write and test user register route

--- models/User.js
+++ models/User.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-    email: {
-        type: String
-    },
-    password: { type: String },
-    firstname: { type: String },
-    lastname: { type: String }
+    email: String,
+    password: String,
+    firstname: String,
+    lastname: String
 })
 
 const User = mongoose.model('users', UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -7,27 +7,33 @@ const User = require('../../models/User');
 router.post("/register", (req, res) => {
     User.findOne({email: req.body.email})
     .then(user => {
-        console.log("finished looking")
-        console.log(user)
-    //     if(user) {
-    //         res.status(400).json({email: "The email is already in use"})
-    //     } else {
-    //             const newUser = new User({
-    //                 email: req.body.email,
-    //                 firstname: req.body.firstname,
-    //                 lastname: req.body.lastname,
-    //                 password: req.body.password
-    //             })
-    //             bcrypt.genSalt(10, (err, salt) => {
-    //                 bcrypt.hash(newUser.password, salt, (err, hash) => {
-    //                     if (err) console.log(err);
-    //                     newUser.password = hash;
-    //                     newUser.save()
-    //                         .then(user => res.json(user))
-    //                         .catch(err => console.log(err));
-    //                 })
-    //             })
-            // }
+        if(user) {
+            res.status(400).json({email: "The email is already in use"})
+        } else {
+                const newUser = new User({
+                    email: req.body.email,
+                    firstname: req.body.firstname,
+                    lastname: req.body.lastname,
+                    password: req.body.password
+                })
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) console.log("err1" + err);
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => {
+                                console.log(user);
+                                safeUser = {}
+                                safeUser._id = user._id;
+                                safeUser.firstname = user.firstname,
+                                safeUser.lastname = user.lastname,
+                                safeUser.email = user.email
+                                res.json(safeUser);
+                            })
+                            .catch(err => console.log("err2" + err));
+                    })
+                })
+            }
         })
     // res.json("register user works")
 })
