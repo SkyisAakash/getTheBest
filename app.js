32afeed39ad6dc476a82805395d28b925227b4dc
Aakash Sarang <sarangaakash@gmail.com>
2018-08-29 14:11:24 -0700

    Chech test route

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+
+
+const bodyParser = require('body-parser');
+
 app.get("/", (req, res) => res.send("Hello best app"));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server listening to port ${port}`));
+const users = require("./routes/api/users");
+app.use("/api/users", users); 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
