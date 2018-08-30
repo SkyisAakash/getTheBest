7afa1919c0c5e3fd54a4ad0e9bab1c570c7e0863
Aakash Sarang <sarangaakash@gmail.com>
2018-08-29 19:38:41 -0700

    Make a store for the react app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as APIUtil from './util/session_api_util';
+import configureStore from './store/store';
 import registerServiceWorker from './registerServiceWorker';
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    let store = configureStore();
+    if (localStorage.jwtToken) {
+        APIUtil.setAuthToken(localStorage.jwtToken);
+        const decoded = jwt_decode(localStorage.jwtToken);
+        store.dispatch(APIUtil.setCurrentUser(decoded));
+
+        const currentTime = Date.now() / 1000;
+        if (decoded.exp < currentTime) {
+            store.dispatch(APIUtil.logoutUser());
+            window.location.href = '/login';
+        }
+    }
+})
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
