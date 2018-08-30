d101a28cf264f0e00f34039dcfa5b88780c90381
Aakash Sarang <sarangaakash@gmail.com>
2018-08-29 19:28:04 -0700

    Write sessionReducer

---
+++ frontend/src/reducers/session_reducer.js
@@ -0,0 +1,22 @@
+import { RECEIVE_CURRENT_USER } from '../util/session_api_util';
+
+const _nullUser = Object.freeze({
+    id: null
+});
+
+const sessionReducer = ( state = _nullUser, action) => {
+    Object.freeze(state);
+    switch(action.type) {
+        case RECEIVE_CURRENT_USER:
+            return {
+                id: action.payload.id,
+                firstname: action.payload.firstname,
+                lastname: action.payload.lastname,
+                email: action.payload.email
+            }
+        default:
+            return state;
+    }
+}
+
+export default sessionReducer;
