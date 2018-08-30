2423525a75f6edae807f05356728f8b4c57e9b00
Aakash Sarang <sarangaakash@gmail.com>
2018-08-30 07:22:45 -0700

    Write session form

---
+++ frontend/src/components/session_form/session_form.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+class SessionForm extends React.component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstname: '',
+            lastname: '',
+            password: '',
+            password2: '',
+            email: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    update(field) {
+        return e => this.setState({
+            [field]: e.currentTarget.value
+        });
+    }
+
+    renderErrors() {
+        return(
+            <ul>
+                {this.props.errors.map((error, i) => (
+                    <li key={`error-${i}`}>
+                    {error}
+                    </li>
+                ))}
+                </ul>
+        )
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, this.state);
+        this.props.processForm(user);
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit= {this.handleSubmit}>
+                    please {this.props.formType} or {this.props.navLink}
+                    {this.renderErrors()}
+                    <input type="text"
+                           value={this.state.firstname}
+                           onChange={this.update('firstname')}/>
+                    <input type="text"
+                        value={this.state.firstname}
+                        onChange={this.update('lastname')} />
+                    <input type="text"
+                        value={this.state.firstname}
+                        onChange={this.update('email')} />
+                    <input type="text"
+                        value={this.state.firstname}
+                        onChange={this.update('password')} />
+                    <input type="submit" value={this.props.formType}/>
+                </form>
+            </div>
+        )
+    }
+
+}
