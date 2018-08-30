2aa2c31f74fbd9f81483402efd1d72d9270c24a3
Aakash Sarang <sarangaakash@gmail.com>
2018-08-29 19:22:47 -0700

    Write actions for login and register user

--- frontend/src/util/session_api_util.js
+++ frontend/src/util/session_api_util.js
@@ -12,3 +12,49 @@ export const setAuthToken = token => {
         delete axios.defaults.headers.common['Authorization'];
     }
 };
+
+export const registerUser = (userData, history) => dispatch => {
+    axios.post('/api/users/register', userData)
+         .then( res => {
+             const { token } = res.data;
+             localStorage.setItem('jwtToken', token);
+             setAuthToken(token);
+             const decoded = jwt_decode(token);
+             dispatch(setCurrentUser(decoded));
+         })
+         .catch(err => 
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }));
+};
+
+export const loginUser = userData => dispatch => {
+    axios
+        .post('/api/users/login', userData)
+        .then(res => {
+            const { token } = res.data;
+            localStorage.setItem('jwtToken', token);
+            setAuthToken(token);
+            const decoded = jwt_decode(token);
+            dispatch(setCurrentUser(decoded));
+        })
+        .catch(err => 
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }));
+}
+
+export const setCurrentUser = decoded => {
+    return {
+        type: RECEIVE_CURRENT_USER,
+        payload: decoded
+    };
+};
+
+export const logoutUser = () => dispatch => {
+    localStorage.removeItem('jwtToken');
+    setAuthToken(false);
+    dispatch(setCurrentUser({}));
+}
