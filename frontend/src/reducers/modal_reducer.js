2e7e87b79f9373ece10d7a4ff04b1069ed1b47b7
Aakash Sarang <sarangaakash@gmail.com>
2018-08-30 19:47:06 -0700

    Add stylesheet for modal

--- frontend/src/reducers/modal_reducer.js
+++ frontend/src/reducers/modal_reducer.js
@@ -1,7 +1,7 @@
 import { OPEN_MODAL, CLOSE_MODAL } from '../actions/modal_actions';
 
-export default function modalReducer(State = null, action) {
-    switch (Action.type) {
+export default function modalReducer(state = null, action) {
+    switch (action.type) {
         case OPEN_MODAL:
             return action.modal
         case CLOSE_MODAL:
--- frontend/src/reducers/ui_reducer.js
+++ frontend/src/reducers/ui_reducer.js
@@ -1,4 +1,4 @@
-import { combineReducers } from 'react-redux';
+import { combineReducers } from 'redux';
 import modal from './modal_reducer';
 export default combineReducers({
     modal
