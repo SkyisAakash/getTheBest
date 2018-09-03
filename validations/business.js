3ebc7592ec197222b8b9b8d560d8be6984d502cd
Aakash Sarang <sarangaakash@gmail.com>
2018-09-02 22:51:48 -0700

    Write validation file for business

---
+++ validations/business.js
@@ -0,0 +1,19 @@
+import Validator from 'validator';
+const isEmpty = require('./is-empty');
+
+module.exports = function validateBusinessInput(data) {
+    let errors = {};
+    data.title = !isEmpty(data.title) ? data.title : '';
+    data.address = !isEmpty(data.address) ? data.address : '';
+
+    if(Validator.isEmpty(data.title)) {
+        errors.title = 'Title field is required';
+    } else if (!Validator.isLength(data.title, {min: 2, max: 30})) {
+        errors.title = 'Title must be between 3-30 characters long'
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
