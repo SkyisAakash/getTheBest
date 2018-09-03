b0636be22945629705a69379e9a93532d8b77432
Aakash Sarang <sarangaakash@gmail.com>
2018-09-02 23:15:01 -0700

    Write business reducer

---
+++ frontend/src/reducers/businesses_reducer.js
@@ -0,0 +1,13 @@
+import merge from 'lodash/marge';
+import { CREATE_BUSINESS } from '../actions/business_actions';
+
+const businessesReducer = (state={}, action) => {
+    switch (action.type) {
+        case CREATE_BUSINESS:
+            return merge(state, action.business)
+        default:
+            return state
+    }
+}
+
+export default businessesReducer;
