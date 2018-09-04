8617944dcee1568fb092cb157c4b4bad41a7d9d8
Aakash Sarang <sarangaakash@gmail.com>
2018-09-04 10:53:02 -0700

    Fix bug in production

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('frontend/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
