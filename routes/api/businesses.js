75b89e4d3f59e1f302f7415907b3b54d8000ba8d
Aakash Sarang <sarangaakash@gmail.com>
2018-09-04 20:02:25 -0700

    Write delete and put route for business

--- routes/api/businesses.js
+++ routes/api/businesses.js
@@ -35,4 +35,22 @@ router.post("/register", passport.authenticate('jwt', {session: false}), (req, r
         })
 })
 
+router.put("/:businessId", passport.authenticate('jwt', { session: false}), (req, res) => {
+    const { errors, isValid } = validateBusinessInput(req.body);
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    Business.findOneAndUpdate({ id: req.params.businessId }, req.body, {new: true})
+        .then((business) => res.json({
+            business: business
+        }))
+        .catch(() => res.json("Oops! Something went wrong! :("))
+})
+
+router.delete("/:businessId", passport.authenticate('jwt', {session: false}), (req, res) => {
+    Business.deleteOne({_id: req.params.businessId})
+        .then(() => res.json({business: req.id}))
+})
+
+
 module.exports = router;
