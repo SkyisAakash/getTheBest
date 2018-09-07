2075641190e62bda0c510915aa70f3fe056dcb0c
Aakash Sarang <sarangaakash@gmail.com>
2018-09-06 18:42:03 -0700

    Write service reducer

--- frontend/src/reducers/services_reducer.js
+++ frontend/src/reducers/services_reducer.js
@@ -1,1 +1,14 @@
-// import { GET_CATEGORIES } from '../actions/category_actions'
+import { CREATE_SERVICE } from '../actions/service_actions';
+import merge from 'lodash/merge';
+
+const servicesReducer = (state={}, action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case CREATE_SERVICE:
+            return merge({}, state, {[action.service.id]: action.service});
+        default:
+            return state;
+    }
+}
+
+export default servicesReducer;
