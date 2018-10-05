5949e24a4c569ddcec54d0a7c8af699a2abf1843
Aakash Sarang <sarangaakash@gmail.com>
2018-10-05 00:54:20 -0700

    Experiment with store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,13 +2,23 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
-
-const configureStore = (preloadedState = {}) => (
+const configureStoreDev = (preloadedState = {}) => (
     createStore(
         rootReducer,
         preloadedState,
         applyMiddleware(thunk, logger)
     )
 );
+const configureStoreProd = (preloadedState = {}) => (
+    createStore(
+        rootReducer,
+        preloadedState,
+        applyMiddleware(thunk)
+    )
+);
+let configureStore;
+if (process.env.NODE_ENV === 'production')configureStore=configureStoreProd;
+else configureStore=configureStoreDev;
+
 
 export default configureStore;
